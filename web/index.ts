69e8d5f313f3d6b4c795319a5d54a0cc5c971144
David <davidhodel6@gmail.com>
2019-12-07 10:41:37 +0100

    added node scrips and removed unused dependency to fs

--- web/index.ts
+++ web/index.ts
@@ -1,5 +1,4 @@
 import express = require("express");
-import fs = require("fs");
 
 const app: express.Application = express();
 const port = 8080;
@@ -8,9 +7,4 @@ app.listen(port, () => {
     console.log(`Express server started at port ${port}`);
 });
 
-app.get("/", (req, res) => {
-    fs.readFile("./index.html", (err, data) => {
-        if (err) throw err;
-        res.send(data);
-    })
-});
+app.use("/", express.static("web/"));
